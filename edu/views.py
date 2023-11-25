410c454d8abde40d94f46fc348cad887b4efbd75
Seung8521 <seungyeop@letscoding.kr>
2023-11-25 11:50:18 +0900

    upload_content

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import View
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 class Index(View):
     template_name = 'index.html'
@@ -12,3 +12,18 @@ class TagStudy(View):
 
     def get(self, request):
         return render(request, self.template_name)
+    
+class NewContent(View):
+    template_name = 'upload_form.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        #content 정보를 전달받는다
+        param = request.POST.get('content', '')
+        #화면에서 확인 하기위해 출력
+        print(f"param:{param}")
+        #받는게 완료되면 /edu/tag 로 이동
+        return redirect('edu:tag_study')
+        
