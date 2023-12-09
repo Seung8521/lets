92d321e0d8ac6ec2bfa66be54e5905940b8e37e3
Seung8521 <seungyeop@letscoding.kr>
2023-12-09 11:48:46 +0900

    image upload

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import View
 from django.shortcuts import get_object_or_404, render, redirect
+from .models import Feed
+#현재폴더의 models 라는 파일에서 Feed라는 이름의 class를 가져오세요.
 
 class Index(View):
     template_name = 'index.html'
@@ -22,8 +24,11 @@ class NewContent(View):
     def post(self, request):
         #content 정보를 전달받는다
         param = request.POST.get('content', '')
+        param2 = request.FILES.get('up_photo', False)
         #화면에서 확인 하기위해 출력
         print(f"param:{param}")
+        feed = Feed(content = param, photo = param2)
+        feed.save()
         #받는게 완료되면 /edu/tag 로 이동
         return redirect('edu:tag_study')
         
